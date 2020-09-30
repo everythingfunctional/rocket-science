765af8b38aec20d7370adf437cb0d1aa50d32671
Brad Richardson <everythingfunctional@protonmail.com>
2020-09-30 10:46:25 -0500

    extract reading of the input file out to the main program

--- src/legacy/rocket.f90
+++ src/legacy/rocket.f90
@@ -152,7 +152,22 @@ end subroutine
 !!  Main program
 
 
-function legacy_rocket(input_file)
+function legacy_rocket( &
+    dt_, &
+    t_max_, &
+    c_p_, &
+    MW_, &
+    temperature_, &
+    pressure_, &
+    T_flame_, &
+    r_ref_, &
+    n_, &
+    id_, &
+    od_, &
+    length_, &
+    rho_solid_, &
+    dia_, &
+    C_f_)
   !! this is a basic program of a single stage
   !! rocket motor flowing out of a nozzle, assuming
   !! a thrust coefficient and ignoring the complexities of
@@ -161,55 +176,29 @@ function legacy_rocket(input_file)
 use mod1
 implicit none
 
-character(len=*), intent(in) :: input_file
+real(dp), intent(in) :: dt_, t_max_
+real(dp), intent(in) :: c_p_, MW_
+real(dp), intent(in) :: temperature_, pressure_
+real(dp), intent(in) :: T_flame_, r_ref_, n_
+real(dp), intent(in) :: id_, od_, length_, rho_solid_
+real(dp), intent(in) :: dia_, C_f_
 real(dp), allocatable :: legacy_rocket(:,:)
 
-integer file_unit
-
-real(dp) dt_, t_max_
-real(dp) c_p_, MW_
-real(dp) temperature_, pressure_
-real(dp) T_flame_, r_ref_, n_
-real(dp) id_, od_, length_, rho_solid_
-real(dp) dia_, C_f_
-
-namelist/numerics_list/ dt_, t_max_
-namelist/gas_list/ c_p_, MW_
-namelist/state_list/  temperature_, pressure_
-namelist/combustion_list/ T_flame_, r_ref_, n_
-namelist/grain_list/ id_, od_, length_, rho_solid_
-namelist/nozzle_list/ dia_, C_f_
-
-open(newunit=file_unit, file=input_file, status="old")
-
-read(file_unit, nml=numerics_list)
 dt   = dt_
 tmax = t_max_
-
-read(file_unit, nml=gas_list)
 cp = c_p_
 mw = MW_
-
-read(file_unit, nml=state_list)
 t = temperature_
 p = pressure_
-
-read(file_unit, nml=combustion_list)
 Tflame = T_flame_
 rref   = r_ref_
 n      = n_
-
-read(file_unit, nml=grain_list)
 id     = id_
 od     = od_
 length = length_
 rhos   = rho_solid_
-
-read(file_unit, nml=nozzle_list)
 dia = dia_
 cf  = C_f_
-
-close(file_unit)
 
 
 !  propellent grain is a cylinder burning radially outward and axially inward from one end.
