55851458b0cd5d3b6b692c73158ae9e07dd026f2
Brad Richardson <everythingfunctional@protonmail.com>
2020-10-02 12:22:20 -0500

    Use better names and all caps for constants

--- src/refurbished/rocket.f90
+++ src/refurbished/rocket.f90
@@ -8,19 +8,19 @@ module refurbished
   integer, parameter :: range = 307
   integer, parameter :: dp = selected_real_kind(precision, range)
 
-  real(dp), parameter :: cd = 1.1_dp
-  real(dp), parameter :: gravity = 9.81_dp
-  real(dp), parameter :: mwair = 28.96_dp
-  real(dp), parameter :: one = 1.0_dp
-  real(dp), parameter :: pamb = 101325.0_dp ! atmospheric pressure
-  real(dp), parameter :: pi = 3.1415926539_dp
-  real(dp), parameter :: psipa = 6894.76_dp ! pascals per psi
-  real(dp), parameter :: pref = 3000.0_dp * psipa ! reference pressure
-  real(dp), parameter :: rhob = 1.225_dp
-  real(dp), parameter :: RU = 8314.0_dp
-  real(dp), parameter :: surfrocket = pi / 4.0_dp
-  real(dp), parameter :: tamb = 300.0_dp
-  real(dp), parameter :: zero = 0.0_dp
+  real(dp), parameter :: DRAG_COEFFICIENT = 1.1_dp
+  real(dp), parameter :: GRAVITY = 9.81_dp
+  real(dp), parameter :: MOLECULAR_WEIGHT_OF_AIR = 28.96_dp
+  real(dp), parameter :: ONE = 1.0_dp
+  real(dp), parameter :: ATMOSPHERIC_PRESSURE = 101325.0_dp
+  real(dp), parameter :: PI = 3.1415926539_dp
+  real(dp), parameter :: PASCALS_PER_PSI = 6894.76_dp
+  real(dp), parameter :: REFERENCE_PRESSURE = 3000.0_dp * PASCALS_PER_PSI
+  real(dp), parameter :: REFERENCE_AIR_DENSITY = 1.225_dp
+  real(dp), parameter :: UNIVERSAL_GAS_CONSTANT = 8314.0_dp
+  real(dp), parameter :: ROCKET_SURFACE_AREA = PI / 4.0_dp
+  real(dp), parameter :: AMBIENT_TEMPERATURE = 300.0_dp
+  real(dp), parameter :: ZERO = 0.0_dp
 contains
   subroutine propwt(id, length, od, rhos,  propmass, rocketmass)
     ! calculate weight of propellent
@@ -31,7 +31,7 @@ contains
     real(dp), intent(out) :: propmass
     real(dp), intent(out) :: rocketmass
 
-    propmass = pi / 4.0_dp * (od**2 - id**2) * length * rhos
+    propmass = PI / 4.0_dp * (od**2 - id**2) * length * rhos
     rocketmass = 0.15_dp * propmass ! assume 85% propellant loading and 15% extra wt of rocket
   end subroutine
 
@@ -43,7 +43,7 @@ contains
     real(dp), intent(inout) :: db
     real(dp), intent(out) :: r
 
-    r = rref * (p/pref)**n ! calculate burn rate
+    r = rref * (p/REFERENCE_PRESSURE)**n ! calculate burn rate
     db = db + r*dt ! calculate incremental burn distance
   end subroutine
 
@@ -58,7 +58,7 @@ contains
     real(dp), intent(out) :: r
     real(dp), intent(out) :: surf
 
-    surf = pi * (id + 2.0_dp*db) * (length - 2.0_dp*db) + pi * (od**2 - (id + 2.0_dp*db)**2) * 0.5_dp
+    surf = PI * (id + 2.0_dp*db) * (length - 2.0_dp*db) + PI * (od**2 - (id + 2.0_dp*db)**2) * 0.5_dp
 
     if (id + 2.0_dp*db .gt. od .or. db.gt.length/2.0_dp) then
       surf = 0.0_dp ! we hit the wall and burned out
@@ -107,18 +107,18 @@ contains
     mdotos = 0.0_dp
     edotos = 0.0_dp ! initially set them to zero prior to running this loop
 
-    if (p.GT.pamb) then
+    if (p.GT.ATMOSPHERIC_PRESSURE) then
       dsigng = 1.0_dp
       tx = t
       px = p
       hx = cp * t
-      pratio = p / pamb
+      pratio = p / ATMOSPHERIC_PRESSURE
     else
       dsigng = -1.0_dp
-      tx = tamb
-      px = pamb
-      hx = cp * tamb
-      pratio = pamb / p
+      tx = AMBIENT_TEMPERATURE
+      px = ATMOSPHERIC_PRESSURE
+      hx = cp * AMBIENT_TEMPERATURE
+      pratio = ATMOSPHERIC_PRESSURE / p
     end if
 
     pcrit = (2.0_dp / (g + 1.0_dp))**(g / (g - 1.0_dp))
@@ -183,9 +183,9 @@ contains
 
     real(dp) :: den
 
-    thrust = (p - pamb) * area * cf ! correction to thrust (actual vs vacuum thrust)
-    den = rhob * exp(-gravity * mwair * altitude / RU / tamb)
-    drag = -cd * 0.5_dp * den * vel * abs(vel) * surfrocket
+    thrust = (p - ATMOSPHERIC_PRESSURE) * area * cf ! correction to thrust (actual vs vacuum thrust)
+    den = REFERENCE_AIR_DENSITY * exp(-GRAVITY * MOLECULAR_WEIGHT_OF_AIR * altitude / UNIVERSAL_GAS_CONSTANT / AMBIENT_TEMPERATURE)
+    drag = -DRAG_COEFFICIENT * 0.5_dp * den * vel * abs(vel) * ROCKET_SURFACE_AREA
     netthrust=thrust+drag
   end subroutine
 
@@ -212,7 +212,7 @@ contains
     real(dp), intent(out) :: accel
 
     propmass = propmass - mdotgen*dt ! incremental change in propellant mass
-    accel = netthrust / (propmass + rocketmass + mcham) - gravity
+    accel = netthrust / (propmass + rocketmass + mcham) - GRAVITY
     vel = vel + accel*dt
     altitude = altitude + vel*dt
   end subroutine
@@ -254,12 +254,12 @@ contains
     real(dp), intent(in) :: cf
     real(dp), allocatable :: rocket(:,:)
 
-    real(dp) :: accel = zero
-    real(dp) :: altitude = zero
+    real(dp) :: accel = ZERO
+    real(dp) :: altitude = ZERO
     real(dp) :: area
     real(dp) :: cv
-    real(dp) :: db = zero
-    real(dp) :: drag = zero
+    real(dp) :: db = ZERO
+    real(dp) :: drag = ZERO
     real(dp) :: echam
     real(dp) :: edotgen
     real(dp) :: edotos
@@ -267,20 +267,20 @@ contains
     integer :: i
     real(dp) :: mcham
     real(dp) :: mdotgen
-    real(dp) :: mdotos = zero
-    real(dp) :: netthrust = zero
+    real(dp) :: mdotos = ZERO
+    real(dp) :: netthrust = ZERO
     integer :: nsteps
     real(dp) :: p
-    real(dp) :: propmass = zero
+    real(dp) :: propmass = ZERO
     real(dp) :: rgas
     real(dp) :: r
-    real(dp) :: rocketmass = zero
+    real(dp) :: rocketmass = ZERO
     real(dp) :: surf
     real(dp) :: t
-    real(dp) :: thrust = zero
-    real(dp) :: time = zero
-    real(dp) :: vol = one
-    real(dp) :: vel = zero
+    real(dp) :: thrust = ZERO
+    real(dp) :: time = ZERO
+    real(dp) :: vol = ONE
+    real(dp) :: vel = ZERO
 
     t = temperature_
     p = pressure_
@@ -301,11 +301,11 @@ contains
 
     !! now begin calculating and initializing
     ! gas variables
-    rgas = ru / mw
+    rgas = UNIVERSAL_GAS_CONSTANT / mw
     cv = cp - rgas
     g = cp / cv
 
-    area = pi / 4.0_dp * dia**2.0_dp ! nozzle area
+    area = PI / 4.0_dp * dia**2.0_dp ! nozzle area
 
     ! calculate initial mass and energy in the chamber
     mcham = p * vol / rgas / t ! use ideal gas law to determine mass in chamber
@@ -317,7 +317,7 @@ contains
     do i=1,nsteps
       call burnrate(dt, n, p, rref,  db,  r)
       call calcsurf(db, dt, id, length, od,  vol,  r, surf)
-      call calmdotgen(cp, r, rhos, surf, Tflame,  edotgen, mdotgen) ! [mdot,engy,dsign]= massflow(p1,pamb,t1,tamb,cp,cp,rgas,rgas,g,g,area)
+      call calmdotgen(cp, r, rhos, surf, Tflame,  edotgen, mdotgen) ! [mdot,engy,dsign]= massflow(p1,ATMOSPHERIC_PRESSURE,t1,AMBIENT_TEMPERATURE,cp,cp,rgas,rgas,g,g,area)
       call massflow(area, cp, g, p, rgas, t,  edotos, mdotos)
       call addmass(dt, edotgen, edotos, mdotgen, mdotos,  echam, mcham)
       call calct(cv, echam, mcham,  t)
